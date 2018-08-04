462a0755dd360163464ab8b11866ddf2a8467b4b
Szymon Nowak <sznowak13@gmail.com>
2018-08-04 18:41:02 +0200

    Deleted variables assignments in buy function - Cashing functions dont need to be saved in variables as they are operating on literals

--- cashier.py
+++ cashier.py
@@ -11,15 +11,15 @@ customer_cart = {}
 def buy(pick, quantity = 1, stock = current_stock, cart = customer_cart):
     status = cashing.check_availability(pick, stock)
     if status == 'Available' and quantity <= stock[pick]:
-        customer_cart = cashing.add_to_cart(cart, pick, quantity)
-        current_stock = cashing.remove_from_stock(pick, stock, quantity)
+        cashing.add_to_cart(cart, pick, quantity)
+        cashing.remove_from_stock(pick, stock, quantity)
         print("Item(s) added.")
     elif status == 'Available' and quantity > stock[pick]:
         print("Unfortunately, we don't have that much {}, but we can add the remaining {} to your cart. Kk? (y/n)".format(pick, stock[pick]))
         decision = input(">>")
         if decision.lower() == 'y':
-            customer_cart = cashing.add_to_cart(cart, pick, stock[pick])
-            current_stock = cashing.remove_from_stock(pick, stock, stock[pick])
+            cashing.add_to_cart(cart, pick, stock[pick])
+            cashing.remove_from_stock(pick, stock, stock[pick])
             print("Items added.")
         else:
             print('kthxbai')
@@ -28,30 +28,15 @@ def buy(pick, quantity = 1, stock = current_stock, cart = customer_cart):
     else:
         print("We dont have that item.")
 
-<<<<<<< HEAD
-choosing = True
-customer_cart = {}
-#wypierdoliłem to przed pętle bo mnie wkurwiało za każdym razem wyskakiwanie tego
-#i sformatowałem to ładniej.
-help_note = """
-=======
 def print_help():
     print("""
->>>>>>> 2bfc0d452d3ef21b4eda832bd972892eef7f7bde
 Enter an item that you want to add to your cart.
 You can also type in a quantity of that item separated by space,
 if you want to add more than one, e.g.: 'Tomato 3' adds 3 tomatoes.
 To see your cart - type 'show cart'.
 To see the stock - type 'show stock'.
-<<<<<<< HEAD
-To exit - type 'exit'.
-To see this message - type 'help'.
-"""
-print(help_note)
-=======
 To see this message again - type 'help'.
 To exit - type 'exit'.""")
->>>>>>> 2bfc0d452d3ef21b4eda832bd972892eef7f7bde
 
 choosing = True
 print_help()
@@ -61,11 +46,7 @@ while choosing:
     if pick == "exit":
         choosing = False
     elif pick == "help":
-<<<<<<< HEAD
-        print(help_note)
-=======
         print_help()
->>>>>>> 2bfc0d452d3ef21b4eda832bd972892eef7f7bde
     elif pick == "show cart":
         cashing.show_cart_and_value(customer_cart)
     elif pick == "show stock":
