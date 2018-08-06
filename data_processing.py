95313bae3766e161ceb55ce46fbdceb9bac306c0
Gildour <davidgildour@gmail.com>
2018-08-06 19:35:16 +0200

    refactored the add function a bit

--- data_processing.py
+++ data_processing.py
@@ -1,6 +1,6 @@
-path = "./data/stock.csv"
+stock_path = "./data/stock.csv"
 
-def read_stock(path = path):
+def read_stock(path = stock_path):
     with open(path) as f:
         records = f.readlines()
         output = {}
@@ -9,7 +9,7 @@ def read_stock(path = path):
             output[vals[1]] = int(vals[2])
         return output
 
-def read_values(path = path):
+def read_values(path = stock_path):
     with open(path) as f:
         records = f.readlines()
         output = {}
@@ -18,7 +18,7 @@ def read_values(path = path):
             output[vals[1]] = float(vals[3])
         return output
 
-def add_item(path = path):
+def add_item(name, quantity, value, path = stock_path):
     with open(path, "r+") as f:
         records = f.readlines()
 
@@ -26,11 +26,15 @@ def add_item(path = path):
             id = "0" + str(len(records) + 1)
         else:
             id = str(len(records) + 1)
-        name = input("Product name: ")
-        quantity = input("Quantity: ")
-        value = input("Value: ")
 
         record = [id, name.upper(), quantity, value, "\n"]
         f.write(",".join(record))
 
-        print("{} {} added!".format(quantity, name))
+def remove_item(path = stock_path):
+    return None
+
+def edit_item(path = stock_path):
+    return None
+
+def show(path = stock_path):
+    return None
--- stock_manager.py
+++ stock_manager.py
@@ -10,7 +10,17 @@ while True:
         print("Goodbye!")
         break;
     elif usr_inpt.lower() == "add":
-        data.add_item()
+        name = input("Product name: ")
+        quantity = input("Quantity: ")
+        value = input("Value: ")
+        data.add_item(name, quantity, value)
+        print("{} {} added!".format(quantity, name))
+    elif usr_inpt.lower() == "remove":
+        data.remove_item() # TO DO
+    elif usr_inpt.lower() == "edit":
+        data.edit_item() # TO DO
+    elif usr_inpt.lower() == "show":
+        data.show() # TO DO
     else:
         print("Unknown command '{}', try again.".format(usr_inpt))
     usr_inpt = input("# ")
