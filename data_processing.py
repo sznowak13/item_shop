999fa163b590fa98f414d39c69f215be28b89047
Gildour <davidgildour@gmail.com>
2018-08-07 20:03:27 +0200

    implemented csv module to the add_item()

--- data_processing.py
+++ data_processing.py
@@ -1,5 +1,6 @@
 import csv, ui
 STOCK_PATH = "./data/stock.csv"
+FIELDNAMES = ["ID", "NAME", "QUANTITY", "VALUE"]
 
 def read_record(item_id, path = STOCK_PATH):
     with open(path) as f:
@@ -23,22 +24,16 @@ def read_values(path = STOCK_PATH):
             output[record["NAME"]] = float(record["VALUE"])
         return output
 
-def add_item(name, quantity, value, path = STOCK_PATH):
-    with open(path, "r+") as f:
-        records = f.readlines()
-
-        if len(records) < 9:
-            item_id = "0" + str(len(records) + 1)
-        else:
-            item_id = str(len(records) + 1)
-
-        record = [item_id, name.upper(), quantity, value]
-        f.write(",".join(record))
+def add_item(item_dict, path = STOCK_PATH):
+    with open(path, "a") as f:
+        fieldnames = FIELDNAMES;
+        writer = csv.DictWriter(f, fieldnames, lineterminator = '\n')
+        writer.writerow(item_dict)
 
 def remove_item(item_id, path = STOCK_PATH):
     with open(path, "r+") as f:
         records = f.readlines()
-        records.pop(int(item_id) - 1)
+        records.pop(int(item_id))
 
         f.seek(0)
         f.truncate()
@@ -53,8 +48,8 @@ def show(path = STOCK_PATH):
     with open(path, "r+") as f:
         records = csv.reader(f)
         records_list = [record for record in records]
-        header = records_list[0]
-        for i in range(1, len(header)):
-            header[i] = header[i].title()
+        header = []
+        for column_name in records_list[0]:
+            header.append(column_name.title())
         records_list.pop(0)
         ui.print_table(records_list, header)
